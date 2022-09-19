dc1bea4ceb392013e475aeef0f2a43cd81dfffe7
junzhou <3306299233@qq.com>
2022-09-19 20:49:22 +0800

    chore(custom): 配置vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    prot:8888,//端口号
+    host:'localhost',//主机名127.0.0.1
+    https:false,//协议
+    open:true//启动服务是否自动打开浏览器
+  },
+  lintOnSave:false,//关闭严格检查
+  productionSourceMap:false,//打包时不会生成.map文件，加快打包速度
+
+
 })
